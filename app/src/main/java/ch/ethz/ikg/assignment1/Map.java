f882a92b2418e6a2e4be023310514e3cedd50ca0
chregi91 <ch.schoenenberger@outlook.com>
2017-04-18 21:58:39 +0200

    Assignment 2 - 16.04.2017

--- app/src/main/java/ch/ethz/ikg/assignment1/Map.java
+++ app/src/main/java/ch/ethz/ikg/assignment1/Map.java
@@ -21,20 +21,30 @@ import com.esri.arcgisruntime.security.UserCredential;
 import com.esri.arcgisruntime.symbology.PictureMarkerSymbol;
 import com.esri.arcgisruntime.symbology.SimpleRenderer;
 
+/**
+ * This activity contains the a Map to navigate. With a long press on the shown features, the user
+ * can navigate to certain POIs.
+ */
 
 public class Map extends AppCompatActivity {
 
     GraphicsOverlay graphicsOverlay = new GraphicsOverlay();
 
+    /**
+     * Creates activity and initializes the necessary view and buttons.
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routing);
 
-        // initialize map
+        // initialize MapView with Map including basemap and graphicsOverlay
         final MapView mapView = (MapView) findViewById(R.id.mapView);
         ArcGISMap map = new ArcGISMap(Basemap.Type.LIGHT_GRAY_CANVAS_VECTOR, 47.408570, 8.506846, 16);
         mapView.setMap(map);
+        mapView.getGraphicsOverlays().add(graphicsOverlay);
 
         // add portal and layers
         Portal portal = this.addPortal();
@@ -42,6 +52,7 @@ public class Map extends AppCompatActivity {
 
         // display current location on map with autopan
         final LocationDisplay locationDisplay = mapView.getLocationDisplay();
+
         // as soon as user pans the AutoPanMode is set to OFF
         locationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.COMPASS_NAVIGATION);
         locationDisplay.startAsync();
@@ -60,30 +71,48 @@ public class Map extends AppCompatActivity {
             }
         });
 
+        // initialize button to adjust view to north
         Button northButton = (Button) findViewById(R.id.northButton);
         northButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mapView.setViewpointRotationAsync(0);
+                // if user is currently navigating just set north and continue without adjusting
+                // map angle to user heading
+                if (locationDisplay.getAutoPanMode() == LocationDisplay.AutoPanMode.COMPASS_NAVIGATION) {
+                    mapView.setViewpointRotationAsync(0);
+                    locationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.COMPASS_NAVIGATION);
+                    // if user is not currently in navigation mode just rotate map north
+                } else {
+                    mapView.setViewpointRotationAsync(0);
+                }
             }
         });
 
+        // create onTouchlisteer which reacts to longpress to start navigating
         mapView.setOnTouchListener(new Routing(this, mapView));
     }
 
+    /**
+     * add the IKG portal with specified credentials
+     * @return
+     */
     public Portal addPortal() {
-        // add portal
         Portal portal = new Portal("http://www.arcgis.com");
         Credential creds = new UserCredential(getString(R.string.arcGIS_user_loc), getString(R.string.arcGIS_password_loc));
         portal.setCredential(creds);
         return portal;
     }
 
+    /**
+     * Add layers to created Portal containing rental, pumping stations and parking lots
+     * @param portal which was created beforehand
+     * @param mapView on which layers are to be displayes
+     */
     public void addLayers(Portal portal, MapView mapView) {
+        // get map on which layers are to be displayed
         ArcGISMap map = mapView.getMap();
-        mapView.getGraphicsOverlays().add(graphicsOverlay);
 
-        // add additional layers from portal
+        // add parking spot layer with adjusted symbology and render features on map
         PortalItem parkingSpot = new PortalItem(portal, "3e96e04f176347c9acd97cdebcf63ea0");
         FeatureLayer parkingSpotLayer = new FeatureLayer(parkingSpot, 0);
         BitmapDrawable parkingDrawable = (BitmapDrawable) ContextCompat.getDrawable(this, R.mipmap.ic_parking);
@@ -94,6 +123,7 @@ public class Map extends AppCompatActivity {
         parkingSpotLayer.setRenderer(rendererParking);
         map.getOperationalLayers().add(parkingSpotLayer);
 
+        // add pumping station layer with adjusted symbology and render features on map
         PortalItem pumpingStation = new PortalItem(portal, "039bcfd424b946e9b799d8c6a81be9ff");
         FeatureLayer pumpingStationLayer = new FeatureLayer(pumpingStation, 0);
         BitmapDrawable pumpDrawable = (BitmapDrawable) ContextCompat.getDrawable(this, R.mipmap.ic_pump);
@@ -104,6 +134,7 @@ public class Map extends AppCompatActivity {
         pumpingStationLayer.setRenderer(rendererPump);
         map.getOperationalLayers().add(pumpingStationLayer);
 
+        // add rental places layer with adjusted symbology and render features on map
         PortalItem rental = new PortalItem(portal, "e12ad29f628a4070b9cacd729a492f4d");
         FeatureLayer rentalLayer = new FeatureLayer(rental, 0);
         BitmapDrawable rentalDrawable = (BitmapDrawable) ContextCompat.getDrawable(this, R.mipmap.ic_rental);
@@ -116,7 +147,7 @@ public class Map extends AppCompatActivity {
     }
 
     /**
-     * This method calls the onStart methods of all observables.
+     * This method calls the onStart methods of the superclass.
      */
     @Override
     protected void onStart() {
@@ -124,8 +155,7 @@ public class Map extends AppCompatActivity {
     }
 
     /**
-     * This function prevents is called when the activity is stopped. The observables are
-     * stopped as well.
+     * This function is called when the activity is stopped.
      */
     @Override
     protected void onStop() {
@@ -133,17 +163,17 @@ public class Map extends AppCompatActivity {
     }
 
     /**
-     * Pauses all the app and also the observables that can be paused.
+     * Pauses all the app.
      */
     protected void onPause() {
         super.onPause();
     }
 
     /**
-     * The method is called, when the activity is resumed. The onResume() Method of the Observables
-     * is called as well.
+     * The method is called, when the activity is resumed.
      */
     protected void onResume() {
         super.onResume();
     }
+
 }
--- app/src/main/java/ch/ethz/ikg/assignment1/Routing.java
+++ app/src/main/java/ch/ethz/ikg/assignment1/Routing.java
@@ -1,21 +1,29 @@
 package ch.ethz.ikg.assignment1;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.Toast;
 
 import com.esri.arcgisruntime.concurrent.ListenableFuture;
 import com.esri.arcgisruntime.data.Feature;
+import com.esri.arcgisruntime.geometry.GeometryEngine;
 import com.esri.arcgisruntime.geometry.Point;
 import com.esri.arcgisruntime.mapping.GeoElement;
 import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener;
 import com.esri.arcgisruntime.mapping.view.Graphic;
 import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.IdentifyLayerResult;
+import com.esri.arcgisruntime.mapping.view.LocationDisplay;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.security.Credential;
 import com.esri.arcgisruntime.security.UserCredential;
@@ -27,29 +35,48 @@ import com.esri.arcgisruntime.tasks.networkanalysis.RouteTask;
 import com.esri.arcgisruntime.tasks.networkanalysis.Stop;
 
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 import static android.content.ContentValues.TAG;
 
 /**
- * Created by chsch on 17.04.2017.
+ * This class handles all the routing. If a user presses on a feature which is contained in the
+ * layers he can navigate to it.
  */
 
 public class Routing extends DefaultMapViewOnTouchListener {
 
+    // initialize needed variables
     private Context mContext = null;
+    private Point destination = null;
+    private Graphic routeGraphic = null;
+    private GraphicsOverlay graphicsOverlay = mMapView.getGraphicsOverlays().get(0);
 
-    // provide a default constructor
-    public Routing(Context context, MapView mapView) {
+    /**
+     * Default constructor
+     *
+     * @param context from activity which calls the routing task
+     * @param mapView from activity which calls the routing task
+     */
+    Routing(Context context, MapView mapView) {
         super(context, mapView);
         mContext = context;
     }
 
-    // override the onSingleTapConfirmed gesture to handle a single tap on the MapView
+    /**
+     * override the onSingleTapConfirmed gesture to handle a single tap on the MapView
+     *
+     * @param e MotionEvent which starts the routing
+     */
     @Override
     public void onLongPress(MotionEvent e) {
+        // if another routing task already exists, clear route
+        if (destination != null) {
+            stopRouting();
+            Toast.makeText(mContext, "Previous routing task cancelled", Toast.LENGTH_SHORT).show();
+        }
+
         // get the screen point where user tapped
         android.graphics.Point screenPoint = new android.graphics.Point((int) e.getX(), (int) e.getY());
         final ListenableFuture<List<IdentifyLayerResult>> identifyFuture = mMapView.identifyLayersAsync(screenPoint, 20, false, 25);
@@ -59,18 +86,21 @@ public class Routing extends DefaultMapViewOnTouchListener {
                 try {
                     // get the identify results from the future - returns when the operation is complete
                     List<IdentifyLayerResult> identifyLayersResults = identifyFuture.get();
-                    // iterate all the layers in the identify result
-                    for (IdentifyLayerResult identifyLayerResult : identifyLayersResults) {
-                        // iterate each result in each identified layer, and check for Feature results
+                    // select topmost layer
+                    if (identifyLayersResults.size() > 0) {
+                        IdentifyLayerResult identifyLayerResult = identifyLayersResults.get(0);
+                        // select topmost element of topmost layer
                         if (identifyLayerResult.getElements().size() > 0) {
                             GeoElement topmostElement = identifyLayerResult.getElements().get(0);
                             if (topmostElement instanceof Feature) {
+                                // get geometry of feature and ask for route to that feature
                                 Feature identifiedFeature = (Feature) topmostElement;
-                                Point to = (Point) identifiedFeature.getGeometry();
-                                askForRoute(to);
+                                destination = (Point) identifiedFeature.getGeometry();
+                                askForRoute(destination);
                             }
                         }
                     }
+
                 } catch (InterruptedException | ExecutionException ex) {
                     Log.e(TAG, "Exception: Wrong features retrieved", ex);
                     ex.printStackTrace();
@@ -79,7 +109,12 @@ public class Routing extends DefaultMapViewOnTouchListener {
         });
     }
 
-    public void askForRoute(final Point to) {
+    /**
+     * Ask user if he wants to route to a certain POI
+     *
+     * @param to point to which the route leads
+     */
+    private void askForRoute(final Point to) {
         DialogInterface.OnClickListener dialogClickListener = new
                 DialogInterface.OnClickListener() {
                     @Override
@@ -89,26 +124,37 @@ public class Routing extends DefaultMapViewOnTouchListener {
                                 route(mMapView, mMapView.getLocationDisplay().getMapLocation(), to);
                                 break;
                             case DialogInterface.BUTTON_NEGATIVE:
-                                Toast.makeText(mContext, "Map canceled!", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(mContext, "Routing canceled!", Toast.LENGTH_SHORT).show();
+                                // clear destination if routing task is cancelled
+                                destination = null;
                                 break;
                         }
                     }
                 };
         AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
         DecimalFormat df = new DecimalFormat("#.000");
-        builder.setMessage("Do you want to route to (" + df.format(to.getX()) +
-                ", " + df.format(to.getY()) + ")?")
+        builder.setMessage("Do you want to route to " + df.format(to.getX()) +
+                ", " + df.format(to.getY()) + "?")
                 .setPositiveButton("Yes", dialogClickListener)
                 .setNegativeButton("No", dialogClickListener).show();
+        // add LocationListener which stops routing task when destination is reached
+        addDestinationAwareness();
     }
 
+    /**
+     * Routing task itself which accesses the routing service and displays the route on the provided
+     * MapView
+     * @param mapView on which the route is to be displayed
+     * @param from Origin = current location
+     * @param to destination of routing task
+     */
     private void route(final MapView mapView, final Point from, final Point to) {
-
-        // select routing service and enter credentials
+        // select routing service and enter credentials for service
         final RouteTask routeTask = new RouteTask("http://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World");
         Credential creds = new UserCredential(mContext.getString(R.string.arcGIS_user_loc), mContext.getString(R.string.arcGIS_password_loc));
         routeTask.setCredential(creds);
 
+        // get route parameters
         final ListenableFuture<RouteParameters> routeParametersFuture = routeTask.createDefaultParametersAsync();
         routeParametersFuture.addDoneListener(new Runnable() {
             @Override
@@ -137,10 +183,8 @@ public class Routing extends DefaultMapViewOnTouchListener {
                             RouteResult routeResult = routeResultFuture.get();
                             Route route = routeResult.getRoutes().get(0);
                             SimpleLineSymbol routeSymbol = new SimpleLineSymbol(SimpleLineSymbol.Style.DASH, Color.BLUE, 4);
-                            Graphic routeGraphic = new Graphic(route.getRouteGeometry(), routeSymbol);
-                            GraphicsOverlay graphicsOverlay = mMapView.getGraphicsOverlays().get(0);
+                            routeGraphic = new Graphic(route.getRouteGeometry(), routeSymbol);
                             graphicsOverlay.getGraphics().add(routeGraphic);
-
                         } catch (InterruptedException | ExecutionException e) {
                             e.printStackTrace();
                         }
@@ -148,6 +192,76 @@ public class Routing extends DefaultMapViewOnTouchListener {
                 });
             }
         });
+    }
+
+    /**
+     * This method adds a LocationListener for the current map location. The advantage of including
+     * this method here is that we don't have to worry about the coordinate system of the destination
+     * and the current location.
+     */
+    private void addDestinationAwareness() {
+        mMapView.getLocationDisplay().addLocationChangedListener(new LocationDisplay.LocationChangedListener() {
+            @Override
+            public void onLocationChanged(LocationDisplay.LocationChangedEvent locationChangedEvent) {
+                if (destination != null) {
+                    // if distance between user location and destination is less than 150 map units
+                    // the routing task is stopped and a notification is sent to the user
+                    if (GeometryEngine.distanceBetween(destination, mMapView.getLocationDisplay().getMapLocation()) < 150) {
+                        stopRouting();
+                        sendNotification(mContext, "Destination reached");
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * This method deletes the displayed route from the map and sets the destination to null.
+     */
+    private void stopRouting() {
+        graphicsOverlay.getGraphics().clear();
+        destination = null;
+    }
+
+    /**
+     * Posts a notification in the notification bar when a transition is detected.
+     * If the user clicks the notification, control goes to the MapActivity.
+     */
+    private void sendNotification(Context context, String notificationDetails) {
+        // Create an explicit content Intent that starts the main Activity.
+        Intent notificationIntent = new Intent(context, Map.class);
+
+        // Construct a task stack.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+
+        // Add the main Activity to the task stack as the parent.
+        stackBuilder.addParentStack(MainActivity.class);
+
+        // Push the content Intent onto the stack.
+        stackBuilder.addNextIntent(notificationIntent);
+
+        // Get a PendingIntent containing the entire back stack.
+        PendingIntent notificationPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Get a notification builder that's compatible with platform versions >= 4
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+
+        // Define the notification settings.
+        builder.setSmallIcon(R.drawable.ic_notification)
+                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher))
+                .setColor(Color.RED)
+                .setContentTitle(notificationDetails)
+                .setContentText(context.getString(R.string.proximity_text))
+                .setContentIntent(notificationPendingIntent);
+
+        // Dismiss notification once the user touches it.
+        builder.setAutoCancel(true);
+
+        // Get an instance of the Notification manager
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Issue the notification
+        mNotificationManager.notify(0, builder.build());
     }
 }
 
